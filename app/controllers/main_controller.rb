905466fba4558f5fbe1c93ae3d28045199ba94d6
Dan Parks <djp@ubuntu.ubuntu-domain>
2009-11-21 19:27:10 -0500

    added dummy views for customers, developers

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,24 @@
 class MainController < ApplicationController
+
   def index
   end
-  
-  def login
+
+  def customer
+  end
+
+  def customer_account
+  end
+
+  def customer_backlog
+  end
+
+  def developer
+  end
+
+  def developer_account
+  end
+
+  def developer_projects
   end
 
 end
