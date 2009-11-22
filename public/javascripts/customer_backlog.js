1406186dd5b51b6209f50c28c6ae42580c1cd5dc
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2009-11-21 21:38:28 -0500

    jquery forms

--- public/javascripts/customer_backlog.js
+++ public/javascripts/customer_backlog.js
@@ -10,12 +10,24 @@ jQuery.fn.saveCategory = function() {
   this.click(function() {
     var category_value = $("#category_field").val();
     $("#category_entry").hide();
-    $("#backlog_item").show();       
+    $("#backlog_item").show();
+    $("#category").append("<h3>" + category_value + "</h3>");
     return false;
   });  
+};
+
+jQuery.fn.saveBacklog = function() {
+  this.click(function() {
+    var description = $("#item_description").val();
+    $("#backlog").append("<p>" + description + "</p>");
+    $("#item_description").val("");
+    $("#item_note").val("");
+    return false;
+  });
 };
 
 $(document).ready(function() {
  $(".add_category").addCategory();
  $("#save_category").saveCategory();
+ $("#save_backlog").saveBacklog();
 });
